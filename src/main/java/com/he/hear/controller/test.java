71abd62d8d0118e9b22083305a616420983964d8
hefeng <245998875@qq.com>
2019-01-28 15:42:48 +0800

    重新

---
+++ src/main/java/com/he/hear/controller/test.java
@@ -0,0 +1,17 @@
+package com.he.hear.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("test")
+@Controller
+public class test {
+
+    @RequestMapping("hear")
+    public String hear(){
+        return "";
+    }
+
+
+
+}
