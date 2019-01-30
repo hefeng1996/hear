94118d401bcbc01de99c47eca656ee354afb0521
hefeng <245998875@qq.com>
2019-01-30 17:25:05 +0800

    iframe及跳转

--- src/main/java/com/he/hear/controller/test.java
+++ src/main/java/com/he/hear/controller/test.java
@@ -3,23 +3,29 @@ package com.he.hear.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("test")
+@RequestMapping("/test")
 @Controller
 public class test{
 
-    @RequestMapping("hear")
+    @RequestMapping("/hear")
     public String hear(){
         return "html/hear";
     }
 
-    @RequestMapping("index")
+    @RequestMapping("/index")
     public String index(){
         return "html/index";
     }
 
-    @RequestMapping("index1")
+    @RequestMapping("/index1")
     public String index1(){
         return "html/index1";
+    }
+
+
+    @RequestMapping("/login")
+    public String login(){
+        return "html/login";
     }
 
 }
